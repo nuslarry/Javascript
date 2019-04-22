915720bb5415855a0e282baf8af1124ddfcca877
Cheng Liang Yu <chengly3@uci.edu>
2019-04-22 12:28:25 -0700

    Update and rename 10. hoisting to 10. hoisting.js

--- 10. hoisting.js
+++ 10. hoisting.js
@@ -16,3 +16,15 @@ function foo(){
 foo();
 console.log(age);
 </script>
+
+//example 2:
+<script>
+var age=40
+function foo(){
+    console.log(age) //undefined, 因為age在這一層中被decalre,但尚未賦值
+    var age=65;
+    console.log(age);
+}
+foo();
+console.log(age);
+</script>
